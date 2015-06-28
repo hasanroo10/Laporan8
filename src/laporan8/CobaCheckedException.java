7b2209b39e6cc5ad4c9ba364b5c58465c0e36000
hasanroo10 <60200112015@uin-alauddin.ac.id>
2015-06-28 22:01:42 +0800

    versi 1.0

---
+++ src/laporan8/CobaCheckedException.java
@@ -0,0 +1,26 @@
+package laporan8;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CobaCheckedException {
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream file = new FileInputStream("c:/coba.txt");
+			file.close();
+		}
+		//catch (FileNotFoundException e){
+			//System.out.println("FileNotFoundException terjadi");
+		//}
+		//catch (IOException e){
+			//System.out.println("exception terjadi");
+			
+		//}
+		catch (Exception e){
+			System.out.println("exception terjadi");
+		}
+	}
+
+}
